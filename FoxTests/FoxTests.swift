40443fd10c680087b1a63a6307b6b16f17ebcd7e
Mark <mark@MacBook-Pro-Mark.local>
2024-09-26 13:02:51 +0300

    change structure

---
+++ FoxTests/FoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoxTests.swift
+//  FoxTests
+//
+//  Created by Mark on 25.09.2024.
+//
+
+import Testing
+@testable import Fox
+
+struct FoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
